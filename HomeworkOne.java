4f3af263b416471230d7935bc94dc8f400c80a06
Timon Nadarevic <timon-nadarevic@outlook.com>
2018-11-08 16:43:45 -0600

    HomeworkOne
    First assignment of MIS 207

---
+++ HomeworkOne.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homeworkone;
+
+/**
+ *
+ * @author timon
+ */
+public class HomeworkOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(128 + 64 + 32 + 16 + 8 + 4 + 2 + 1);
+    }
+    
+}
